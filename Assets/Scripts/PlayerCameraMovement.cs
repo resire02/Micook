6e49097f535d8a0a69ff22dde9be65f5f333787d
resire02 <65257747+resire02@users.noreply.github.com>
2023-07-07 17:26:19 -0700

    adjusted microwave pov

--- Assets/Scripts/PlayerCameraMovement.cs
+++ Assets/Scripts/PlayerCameraMovement.cs
@@ -27,7 +27,7 @@ public class PlayerCameraMovement : MonoBehaviour
 
         yRotation += mouseX;
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -45f, 45f);
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
